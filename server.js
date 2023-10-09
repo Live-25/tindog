2e154de867e13e821221621ecd3ce9ef73167a9a
Live-25 <liveleen25@gmail.com>
2023-10-09 11:36:53 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express= require("express");
+const bodyParser= require("body-parser");
+const request= require("request");
+
+const app=express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.render("index.ejs");
+});
+
+app.get("/index.ejs", function(req,res){
+    res.render("index.ejs");
+});
+
+app.get("/pricing.ejs", function(req,res){
+    res.render("pricing.ejs");
+});
+
+app.get("/contact.ejs", function(req,res){
+    res.render("contact.ejs");
+});
+
+app.get("/dog-care.ejs", function(req,res){
+    res.render("dog-care.ejs");
+});
+
+app.listen(3000, function(){
+    console.log("server is running");
+});
